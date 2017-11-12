624098c1e2418df310b803a77ea153ca96ad61dc
ToastinYou <toastinyou@gmx.com>
2017-11-12 11:26:05 -0500

    Initial commit

---
+++ client.lua
@@ -0,0 +1,15 @@
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+
+		local ped = GetPlayerPed(-1)
+
+		if DoesEntityExist(ped) and IsPedInAnyVehicle(ped, false) and IsControlPressed(2, 75) and not IsEntityDead(ped) and not IsPauseMenuActive() then
+			Citizen.Wait(1000)
+			if DoesEntityExist(ped) and not IsPedInAnyVehicle(ped, false) and not IsEntityDead(ped) and not IsPauseMenuActive() then
+				local veh = GetVehiclePedIsIn(ped, true)
+				SetVehicleEngineOn(veh, true, true, true)
+			end
+		end
+	end
+end)
