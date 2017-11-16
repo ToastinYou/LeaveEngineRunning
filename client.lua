0e2d5ea61b1f35ace9fa7c83608ff7d55010a5d7
ToastinYou <toastinyou@gmx.com>
2017-11-16 15:54:30 -0500

    Fixed engine turning on even if it was off

--- client.lua
+++ client.lua
@@ -3,12 +3,15 @@ Citizen.CreateThread(function()
 		Citizen.Wait(0)
 
 		local ped = GetPlayerPed(-1)
-
+		
 		if DoesEntityExist(ped) and IsPedInAnyVehicle(ped, false) and IsControlPressed(2, 75) and not IsEntityDead(ped) and not IsPauseMenuActive() then
+			local engineWasRunning = GetIsVehicleEngineRunning(GetVehiclePedIsIn(ped, true))
 			Citizen.Wait(1000)
 			if DoesEntityExist(ped) and not IsPedInAnyVehicle(ped, false) and not IsEntityDead(ped) and not IsPauseMenuActive() then
 				local veh = GetVehiclePedIsIn(ped, true)
-				SetVehicleEngineOn(veh, true, true, true)
+				if (engineWasRunning) then
+					SetVehicleEngineOn(veh, true, true, true)
+				end
 			end
 		end
 	end
